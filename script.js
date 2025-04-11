f305d25d700acfbb21b449b8285a50de67c61f07
Aayushma25 <aayushmamehta@gmail.com>
2025-04-11 10:37:34 +0545

    Updated all the files

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let currentSlide = 0;
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlide(index) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-   
-    currentSlide = (index + totalSlides) % totalSlides; 
-
-    const offset = -currentSlide * 100; 
-    slides.style.transform = `translateX(${offset}%)`;
+function changeSlide(n) {
+    showSlides(slideIndex += n);
 }
 
-function changeSlide(direction) {
-    showSlide(currentSlide + direction);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+    const dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
 }
 
 
